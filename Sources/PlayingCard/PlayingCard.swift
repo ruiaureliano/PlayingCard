e9cf774e436d8011bfa2d45451a23c3b3ce9191e
Rui Aureliano <ruiaureliano@gmail.com>
2021-03-24 22:49:14 +0000

    Update

--- Sources/PlayingCard/PlayingCard.swift
+++ Sources/PlayingCard/PlayingCard.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct PlayingCard {
     let rank: Rank
     let suit: Suit
+
+    init(rank: Rank, suit: Suit) {
+        self.rank = rank
+        self.suit = suit
+    }
 }
--- Tests/PlayingCardTests/PlayingCardTests.swift
+++ Tests/PlayingCardTests/PlayingCardTests.swift
@@ -6,7 +6,7 @@ final class PlayingCardTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PlayingCard().text, "Hello, World!")
+       // XCTAssertEqual(PlayingCard().text, "Hello, World!")
     }
 
     static var allTests = [
